3ef5f0acd7904c6a4838c2fad5943bad0cf31584
Nicole Martin <n.martinpdx@gmail.com>
2021-02-11 16:13:32 -0800

    homepage and header style

---
+++ src/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
+import style2 from './Header.module.css'
+
+export default class SearchPage extends Component {
+  render() {
+    return (
+      <header className={style2.headerBox}>
+        <NavLink className={style2.headerLinks} exact activeClassName='selected' to='/'>
+          Home  
+          </NavLink>
+          <NavLink className={style2.headerLinks} exact activeClassName='selected' to='./SearchPage/SearchPage.js'>
+          Search 
+          </NavLink>
+
+      </header>
+    )
+  }
+}
+
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import pika from '../poke-p.webp'
+import style from '../HomePage/HomePage.module.css'
 
 
 export default class HomePage extends Component {
   render() {
     return (
-      <div>
-     
-      HOMEEE
+      <>
+      <section className={style.mainSection}>
+      <div className={style.imgBox}>
+      <img alt="pika" src={pika} />
       </div>
+      </section>
+      </>
     )
   }
 }
